ff86dd374482f66b95550d40af0008301a962240
Yves Guilherme <yvesguilherme2006@gmail.com>
2026-04-17 14:05:23 +0000

    Testes e quick select incompleto

--- aulas/aula04/selecao.h
+++ aulas/aula04/selecao.h
@@ -2,6 +2,8 @@
 #define SELECAO_H
 
 int busca_sequencial(int v[], int n, int valor);
-
+void troca (int *a, int *b);
+int particionar_vetor (int v[], int inicio, int fim);
+int quick_select (int v[], int inicio, int fim);
 
 #endif
