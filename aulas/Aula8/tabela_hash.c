d84313722088ab98b27bb617517fbe0f41480d1e
Yves Guilherme <yvesguilherme2006@gmail.com>
2026-05-22 13:55:17 +0000

    Adiciona Aula8 incompleta.

---
+++ aulas/Aula8/tabela_hash.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "tabela_hash.h"
+
+int calcular_hash(int valor)
+{
+
+}
+
+TabelaHash *criar() 
+{
+
+}
+
+void destruir(TabelaHash *tabela_hash)
+{
+
+}
+
+void inserir(TabelaHash *tabela_hash, int valor)
+{
+
+}
+
+int buscar(TabelaHash *tabela_hash, int valor)
+{
+
+}
+
+void remover(TabelaHash *tabela_hash, int valor)
+{
+
+}
+
+int tabela_vazia(TabelaHash *tabela_hash)
+{
+    
+}
